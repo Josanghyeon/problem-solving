961c652e54bfaad30babb9660ccc271ef25caa36
Josanghyeon <lokijoji4@gmail.com>
2022-07-24 21:20:25 +0900

    7-24

--- BeakJoon/Silver2/1260/1260.cpp
+++ BeakJoon/Silver2/1260/1260.cpp
@@ -3,17 +3,15 @@
 using namespace std;
 #define MAX 1001
 
-int N, M, V;       //정점개수, 간선개수, 시작정점
-int map[MAX][MAX]; //인접 행렬 그래프
-bool visited[MAX]; //정점 방문 여부
+int N, M, V;            //정점개수, 간선개수, 시작정점
+int map[MAX][MAX];      //인접 행렬 그래프
+bool visited[MAX] = {}; //정점 방문 여부
 queue<int> q;
 
 void reset()
 {
     for (int i = 1; i <= N; i++)
-    {
         visited[i] = 0;
-    }
 }
 
 void DFS(int v)
@@ -24,9 +22,8 @@ void DFS(int v)
     for (int i = 1; i <= N; i++)
     {
         if (map[v][i] == 1 && visited[i] == 0)
-        { //현재 정점과 연결되어있고 방문되지 않았으면
+            //현재 정점과 연결되어있고 방문되지 않았으면
             DFS(i);
-        }
     }
 }
 
@@ -40,7 +37,6 @@ void BFS(int v)
     {
         v = q.front();
         q.pop();
-
         for (int w = 1; w <= N; w++)
         {
             if (map[v][w] == 1 && visited[w] == 0)
@@ -65,7 +61,6 @@ int main()
         map[b][a] = 1;
     }
 
-    reset();
     DFS(V);
 
     cout << '\n';
